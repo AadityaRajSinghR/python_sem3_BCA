0253bbc9687751d693527f98ddee2da54d20f754
Aaditya Raj Singh <aaditya.singh@avantika.edu.in>
2024-09-07 09:06:39 +0530

    change 1st file name and add another app.py file

--- app.py
+++ app.py
@@ -1,66 +1,310 @@
-def calculate_average(assignments, exams, projects):
+# Student Grades Calculator App
 
-    # Assignments: 40%, Exams: 50%, Projects: 10%
+# I can declare a Global dictionary here which will be used to store all the criterias
+criteriadic = {}
+TOTAL_WEIGHTAGE = 100
+weightage_list = {}
 
-    weight_assignments = 0.40
-    weight_exams = 0.50
-    weight_projects = 0.10
+def criteria():
+    condition = True
+    global TOTAL_WEIGHTAGE  # Declare TOTAL_WEIGHTAGE as global
     
-    average = (assignments * weight_assignments +
-               exams * weight_exams +
-               projects * weight_projects)
-    return average
-
-def final_grade_fun(average_grade):
-   
-    #Determine the final grade based on the weighted average.
-   
-    if 90 <= average_grade <= 100:
-        return 'A'
-    elif 80 <= average_grade < 90:
-        return 'B'
-    elif 65 <= average_grade < 80:
-        return 'C'
-    elif 50 <= average_grade < 65:
-        return 'D'
-    else:
-        return 'F'
-
-def main():
-    while True:
-        try:
-            num_students = int(input("Enter the number of students: "))
-            if num_students <= 0:
-                print("The number of students must be positive.")
-                continue
-            break
-        except ValueError:
-            print("Invalid input. Please enter an integer value.")
-
-    for _ in range(num_students):
-        student_name = input("Enter Student Name : ")
-        print(f"\nEntering data for student {student_name}:")
-
+    # Criteria Details
+    while condition:
+        
+        # Teacher's Details
+        # subname = input("Enter subject name: ")
+        # instucter = input("Enter teacher name: ")
+        
+        # Ask user if they want to add assignments and how many exam weightages they want to add
+        print("\n--------| Enter Some Exam Details |--------\n")
+        
+        conf = input("Do you want to add Exam? Y/N : ").upper()
+        if conf == "Y":
+            while True:
+                print("\n\nTotal weightage before adding Exam: ", TOTAL_WEIGHTAGE)
+                try:
+                    weightage = int(input("How many exam weightages do you want to add? : "))
+                    if weightage > TOTAL_WEIGHTAGE:   # Check if weightage is less than Total weightage
+                        print(f"Total weightage of Exam cannot be greater than {TOTAL_WEIGHTAGE}%")
+                        continue
+                    
+                    # Minus TOTAL_WEIGHTAGE with weightage
+                    TOTAL_WEIGHTAGE -= weightage
+                    weightage_list["Exam"] = weightage
+                    marks = int(input("How many exam marks do you want to add? : "))
+                    criteriadic["Exam"] = {
+                        "weightage": weightage,
+                        "marks": marks
+                    }
+                    
+                    break
+                except ValueError:
+                    print("Please enter a valid number")
+         
+        print("\n--------| Enter Some Jury Details |--------\n")
+        
+        # And how many Jury weightages they want to add
         while True:
+            print("\n\nTotal weightage before adding Jury: ", TOTAL_WEIGHTAGE, "\n\n")
             try:
-                assignments = float(input("Enter the score for assignments (0-100): "))
-                exams = float(input("Enter the score for exams (0-100): "))
-                projects = float(input("Enter the score for projects (0-100): "))
-
-                if not (0 <= assignments <= 100) or not (0 <= exams <= 100) or not (0 <= projects <= 100):
-                    print("Scores must be between 0 and 100. Please enter again.")
+                weightage = int(input("How many Jury weightages do you want to add? : ")) 
+                if weightage > TOTAL_WEIGHTAGE: # Check if weightage is less than Total weightage
+                    print(f"Total weightage of Jury cannot be greater than {TOTAL_WEIGHTAGE}% because you have only {TOTAL_WEIGHTAGE}% left")
                     continue
+                    
+                # Minus TOTAL_WEIGHTAGE with weightage
+                TOTAL_WEIGHTAGE -= weightage
+                weightage_list["Jury"] = weightage
                 
+                marks = int(input("How many Jury marks do you want to add? : "))
+                criteriadic["Jury"] = {
+                    "weightage": weightage,
+                    "marks": marks
+                }
+                
+                print("\n\nTotal weightage after adding Jury: ", TOTAL_WEIGHTAGE, "\n\n")
                 break
             except ValueError:
-                print("Invalid input. Please enter numerical values.")
+                print("Please enter a valid number")
+        
+        print("\n--------| Enter Some Continuous and Comprehensive Evaluation (CCE) Details |--------\n")
+        
+        while True:
+            print("\n\nTotal weightage before adding CCE: ", TOTAL_WEIGHTAGE, "\n\n")
+        
+            # Ask user if they want to add assignments
+            conf = input("Do you want to add assignments? Y/N : ").upper()
+            if conf == "Y":
+                while True:
+                    try:
+                        num = int(input(f"How many assignments do you want to add? : "))
+                        get_values("Assignment", num)
+                        break
+                    except ValueError:
+                        print("Please enter a valid number")
 
-        average_grade = calculate_average(assignments, exams, projects)
-        final_grade = final_grade_fun(average_grade)
+            # Ask user if they want to add projects
+            conf = input("Do you want to add projects? Y/N : ").upper()
+            if conf == "Y":
+                while True:
+                    try:
+                        num = int(input(f"How many projects do you want to add? : "))
+                        get_values("Project", num)
+                        break
+                    except ValueError:
+                        print("Please enter a valid number")
+                
+            # Ask user if they want to add Quiz
+            conf = input("Do you want to add Quiz? Y/N : ").upper()
+            if conf == "Y":
+                while True:
+                    try:
+                        num = int(input(f"How many Quizzes do you want to add? : "))
+                        get_values("Quiz", num)
+                        break
+                    except ValueError:
+                        print("Please enter a valid number")
+                
+            # Ask user if they want to add Value added course
+            conf = input("Do you want to add Value added course? Y/N : ").upper()
+            if conf == "Y":
+                while True:
+                    try:
+                        num = int(input(f"How many Value added courses do you want to add? : "))
+                        get_values("Value added course", num)
+                        break
+                    except ValueError:
+                        print("Please enter a valid number")
+            
+            # Ask user if they want to add Practical Test
+            conf = input("Do you want to add Practical Test? Y/N : ").upper()
+            if conf == "Y":
+                while True:
+                    try:
+                        num = int(input(f"How many Practical Tests do you want to add? : "))
+                        get_values("Practical Test", num)
+                        break
+                    except ValueError:
+                        print("Please enter a valid number")
+                        
+            conf = input("Do you want to add Attendance? Y/N : ").upper()
+            if conf == "Y":
+                weightage = int(input("Enter Weightage: "))
+                criteriadic["Attendance"] = {
+                    "weightage": weightage,
+                    "marks": 100
+                }
+            
+            while True:
+                conf = input("Do you want to add Other Activity? Y/N : ").upper()
+                if conf == "Y":
+                    while True:
+                        try:
+                            name = input("Enter name of Other Activity: ")
+                            get_values(name, 1)
+                            conf = input("Do you want to add more Other Activities? Y/N : ").upper()
+                            if conf == "N":
+                                break
+                        except ValueError:
+                            print("Please enter a valid value")
+                else:
+                    break
 
-        print(f"\nStudent {student_name}:")
-        print(f"Weighted Average: {average_grade:.2f}")
-        print(f"Final Grade: {final_grade}")
+            break
+                
+        print(f"\n\nCriteriadic: {criteriadic}\n\n")  
+        print(f"Weightage list: {weightage_list}\n\n") 
+        conf = input("Do you want to continue? Y/N : ")
+        if conf == "N" or conf == "n":
+            condition = False
 
-if __name__ == "__main__":
-    main()
+def get_values(name, num):
+    # Ensure '*name' key exists
+    if name not in criteriadic:
+        criteriadic[name] = {}
+
+    # Add *name to the dictionary
+    for i in range(num):
+        # Get {name} details
+        name_name = input(f"Enter name for {name} {i+1}: ")
+        weightage = int(input("Enter Weightage: "))
+        weightage_list[name] = weightage
+        marks = int(input("Enter Marks: "))
+
+        # Add project to the dictionary
+        criteriadic[name][name_name] = {
+            "weightage": weightage,
+            "marks": marks
+        }
+
+
+
+criteria()
+
+
+
+
+
+
+
+
+
+# Student Grades Calculator App
+
+# Global dictionary to store all the criteria
+# criteriadic = {}
+# TOTAL_WEIGHTAGE = 100
+# weightage_list = {}
+
+# def prompt_for_weightage(context):
+#     # Prompt the user for weightage and marks with error handling.
+#     while True:
+#         try:
+#             weightage = int(input(f"Enter weightage for {context}: "))
+#             if weightage <= TOTAL_WEIGHTAGE:
+#                 return weightage
+#             else:
+#                 print(f"Weightage cannot be greater than {TOTAL_WEIGHTAGE}%. Please enter a valid weightage.")
+#         except ValueError:
+#             print("Please enter a valid number.")
+            
+# def prompt_for_CCE_weightage(context):
+#     # Prompt the user for weightage and marks with error handling.
+#     while True:
+#         try:
+#             weightage = int(input(f"Enter weightage for {context}: "))
+#             if weightage <= TOTAL_WEIGHTAGE:
+#                 return weightage
+#             else:
+#                 print(f"Weightage cannot be greater than {TOTAL_WEIGHTAGE}%. Please enter a valid weightage.")
+#         except ValueError:
+#             print("Please enter a valid number.")
+
+# def get_values(name, num):
+#     # Get details for each item type and update the global dictionary.
+#     if name not in criteriadic:
+#         criteriadic[name] = {}
+    
+#     for i in range(num):
+#         name_name = input(f"Enter name for {name} {i+1}: ")
+#         weightage = prompt_for_weightage(name_name)
+#         marks = int(input(f"Enter marks for {name_name}: "))
+        
+#         criteriadic[name][name_name] = {
+#             "weightage": weightage,
+#             "marks": marks
+#         }
+
+# def criteria():
+#     global TOTAL_WEIGHTAGE  # Declare TOTAL_WEIGHTAGE as global
+    
+#     while True:
+#         print("\n--------| Enter Exam Details |--------\n")
+        
+#         if input("Do you want to add Exam? Y/N : ").upper() == "Y":
+#             print(f"Total weightage before adding Exam: {TOTAL_WEIGHTAGE}%")
+#             weightage = prompt_for_weightage("Exam")
+#             weightage_list["Exam"] = weightage
+#             TOTAL_WEIGHTAGE -= weightage
+            
+#             marks = int(input("Enter the number of exam marks: "))
+#             criteriadic["Exam"] = {
+#                 "weightage": weightage,
+#                 "marks": marks
+#             }
+        
+#         print("\n--------| Enter Jury Details |--------\n")
+        
+#         if input("Do you want to add Jury? Y/N : ").upper() == "Y":
+#             print(f"Total weightage before adding Jury: {TOTAL_WEIGHTAGE}%")
+#             weightage = prompt_for_weightage("Jury")
+#             weightage_list["Jury"] = weightage
+#             TOTAL_WEIGHTAGE -= weightage
+            
+#             print(f"\n\nTotal weightage after adding Jury: {TOTAL_WEIGHTAGE}% \n\n")
+            
+#             marks = int(input("Enter the number of jury marks: "))
+#             criteriadic["Jury"] = {
+#                 "weightage": weightage,
+#                 "marks": marks
+#             }
+        
+#         print("\n--------| Enter CCE Details |--------\n")
+#         cce_weightage = 100
+#         if TOTAL_WEIGHTAGE > 0:
+#             weightage = TOTAL_WEIGHTAGE
+#             weightage_list["CCE"] = weightage
+#             # TOTAL_WEIGHTAGE -= weightage
+#             criteriadic["CCE"] = {
+#                 "weightage": weightage,
+#                 "marks": 100  # Assuming full marks are always 100 for CCE
+#             }
+        
+#         for category in ["Assignments", "Projects", "Quiz", "Value added course", "Practical Test"]:
+#             if input(f"\nDo you want to add {category}? Y/N : ").upper() == "Y":
+#                 num = int(input(f"How many {category} do you want to add? : "))
+#                 get_values(category, num)
+        
+#         if input("Do you want to add Attendance? Y/N : ").upper() == "Y":
+#             weightage = prompt_for_CCE_weightage("Attendance")
+#             criteriadic["Attendance"] = {
+#                 "weightage": weightage,
+#                 "marks": 100  # Assuming full marks are always 100 for Attendance
+#             }
+        
+#         while input("Do you want to add Other Activity? Y/N : ").upper() == "Y":
+#             name = input("Enter name of Other Activity: ")
+#             get_values(name, 1)
+        
+#         print("\nFinal Criteria Dictionary:")
+#         print(criteriadic)
+        
+        
+#         print("\nFinal Weightage Dictionary:")
+#         print(weightage_list)
+        
+#         if input("Do you want to continue? Y/N : ").upper() == "N":
+#             break
+
+# criteria()
